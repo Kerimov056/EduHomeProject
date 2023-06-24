dddcbb55db217acdbd0ed441c9fb64752cb4727a
Kerimov056 <ulvisk@code.edu.az>
2023-06-24 04:05:06 -0700

    Ui

--- EduHome.UI/Program.cs
+++ EduHome.UI/Program.cs
@@ -3,12 +3,17 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
-var app = builder.Build();
+
 
 builder.Services.AddDbContext<AppDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 
+
+
+var app = builder.Build();
+
+app.UseStaticFiles();
 app.MapControllerRoute(
        name: "default",
        pattern: "{controller=Home}/{action=Index}/{id?}");
