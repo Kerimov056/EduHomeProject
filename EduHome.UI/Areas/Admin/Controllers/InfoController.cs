abac47767396a628dbf9b0597c23159ac87e1293
Kerimov056 <ulvisk@code.edu.az>
2023-06-25 09:26:28 -0700

    Info Start

---
+++ EduHome.UI/Areas/Admin/Controllers/InfoController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EduHome.UI.Areas.Admin.Controllers;
+
+public class InfoController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
