e028838ec5b0cac0fde36421b27d5eeed989bcf7
Kerimov056 <ulvisk@code.edu.az>
2023-06-26 03:26:25 -0700

    cours create cshtml

---
+++ EduHome.UI/Areas/Admin/Controllers/CoursesController.cs
@@ -0,0 +1,49 @@
+﻿using EduHome.UI.Areas.Admin.Extension;
+using EduHome.UI.Areas.Admin.ViewModel;
+using EduHomeDataAccess.Database;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EduHome.UI.Areas.Admin.Controllers;
+[Area("Admin")]
+public class CoursesController : Controller
+{
+    private readonly AppDbContext _context;
+    public CoursesController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        return View(await _context.Coursess.ToListAsync());
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    //[HttpPost]
+    //[ValidateAntiForgeryToken]
+    //public async Task<IActionResult> Create(CourseFullDetailsViewModel courseFullDetailsViewModel)
+    //{
+    //    if (!ModelState.IsValid)
+    //    {
+    //        return View(courseFullDetailsViewModel);
+    //    }
+    //    if (!courseFullDetailsViewModel.ImagePath.FormatFile("image"))
+    //    {
+    //        ModelState.AddModelError("ImagePath","Select correct image format!");
+    //        return View(courseFullDetailsViewModel);
+    //    }
+    //    if (!courseFullDetailsViewModel.ImagePath.FormatLength(100))
+    //    {
+    //        ModelState.AddModelError("ImagePath", "Size must be less than 100 kb");
+    //        return View(courseFullDetailsViewModel);
+    //    }
+
+
+    //    return RedirectToAction("Index");
+    //}
+}
---
+++ EduHome.UI/Areas/Admin/ViewModel/CourseFullDetailsViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EduHome.UI.Areas.Admin.ViewModel;
+
+public class CourseFullDetailsViewModel
+{
+    public IFormFile ImagePath { get; set; } = null!;
+    public string Cours { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    [Required, MaxLength(50)]
+    public string AboutCours { get; set; } = null!;
+    [Required, MaxLength(350)]
+    public string AboutCoursDescription { get; set; } = null!;
+    [Required, MaxLength(50)]
+    public string ToApply { get; set; } = null!;
+    [Required, MaxLength(350)]
+    public string ToApplyDescription { get; set; } = null!;
+    [Required, MaxLength(50)]
+    public string Certification { get; set; } = null!;
+    [Required, MaxLength(400)]
+    public string CertificationDescription { get; set; } = null!;
+    public DateTime Starts { get; set; }
+    [Required]
+    public int Month { get; set; }
+    [Required]
+    public int Hours { get; set; }
+    [Required, MaxLength(25)]
+    public string Level { get; set; } = null!;
+    [Required, MaxLength(22)]
+    public string Language { get; set; } = null!;
+    [Required]
+    public int Students { get; set; }
+    [Required, MaxLength(20)]
+    public string Assesments { get; set; } = null!;
+    [Required]
+    public int CourseFee { get; set; }
+}
