9d2c65a9386e3e71e74dc90d231c6bd40fd67661
Kerimov056 <ulvisk@code.edu.az>
2023-06-26 01:49:06 -0700

    cours details index iu finsh

--- EduHome.UI/Contollers/CoursDetailsController1.cs
+++ EduHome.UI/Contollers/CoursDetailsController1.cs
@@ -1,19 +1,50 @@
-﻿using EduHomeDataAccess.Database;
+﻿using EduHome.Core.Entities;
+using EduHome.UI.ViewModel;
+using EduHomeDataAccess.Database;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
-namespace EduHome.UI.Contollers;
+namespace EduHome.UI.Controllers;
 
-public class CoursDetailsController1 : Controller
+public class CourseDetailsController : Controller
 {
     private readonly AppDbContext _context;
 
-    public CoursDetailsController1(AppDbContext context)
+    public CourseDetailsController(AppDbContext context)
     {
         _context = context;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index(int id)
     {
-        return View();
+        if (id == 0 || id == null)
+        {
+            return NotFound();
+        }
+        var cart = _context.Coursess.Find(id);
+        if (cart == null)
+        {
+            return NotFound();
+        }
+        ViewBag.Id = cart.Id;
+        //var AllProduc = await _context.Coursess.Include(c => c.CoursesDetails).ToListAsync();
+        HomeViewModel homeViewModel = new()
+        {
+            blogs = await _context.Blogs.ToListAsync(),
+            courses = await _context.Coursess.Include(c => c.CoursesDetails).ToListAsync()
+        };
+        return View(homeViewModel);
     }
 }
+
+
+
+
+
+
+//HomeViewModel homeViewModel = new()
+//{
+//    categories = await _context.Categoriess.ToListAsync(),
+//    courses_details = await _context.CoursesDetailss.ToListAsync(),
+//    courses = await _context.Coursess.ToListAsync()
+//};
