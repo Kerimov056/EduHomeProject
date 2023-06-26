5871c27300c5093e71009852cade98cb0dfd3e36
Kerimov056 <ulvisk@code.edu.az>
2023-06-26 12:41:09 -0700

    Course Create Finsh

--- EduHome.UI/Areas/Admin/Controllers/CoursesController.cs
+++ EduHome.UI/Areas/Admin/Controllers/CoursesController.cs
@@ -120,7 +120,83 @@ public class CoursesController : Controller
 
 
 
+    public async Task<IActionResult> Edit(int id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+
+        ViewBag.catagory = await _context.Categoriess.ToListAsync();
+        Courses course = await _context.Coursess.Include(c => c.CoursesDetails).FirstOrDefaultAsync(n => n.Id == id);
+        if (course == null)
+        {
+            return NotFound();
+        }
+
+        var courseViewModel = _mapper.Map<CourseFullDetailsViewModel>(course);
+
+        return View(courseViewModel);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, CourseFullDetailsViewModel courseViewModel, int CatagoryId)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(courseViewModel);
+        }
+
+        if (id == null)
+        {
+            return BadRequest();
+        }
+
+        var category = await _context.Categoriess.FindAsync(CatagoryId);
+        if (category == null)
+        {
+            ModelState.AddModelError("CatagoryId", "Invalid category selected!");
+            ViewBag.catagory = await _context.Categoriess.ToListAsync();
+            return View(courseViewModel);
+        }
+
+        Courses course = await _context.Coursess.Include(c => c.CoursesDetails).FirstOrDefaultAsync(n => n.Id == id);
+        if (course == null)
+        {
+            return NotFound();
+        }
+
+        _mapper.Map(courseViewModel, course);
+        course.CategoriesId = CatagoryId;
+
+        await _context.SaveChangesAsync();
+
+        return RedirectToAction("Index");
+    }
 
 
-   
+    public async Task<IActionResult> Delete(int id)
+    {
+        if (id == 0 || id == null)
+        {
+            return NotFound();
+        }
+
+        Courses course = await _context.Coursess.Include(c => c.CoursesDetails).FirstOrDefaultAsync(n => n.Id == id);
+        if (course == null)
+        {
+            return NotFound();
+        }
+        return View(course);
+    }
+
+    [HttpPost,ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Delete(int id)
+    {
+        
+    }
+
+
 }
