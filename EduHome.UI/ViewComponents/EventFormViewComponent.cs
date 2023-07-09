7ea9aef8cd30023746115c314447c31d059f1ff8
Kerimov056 <ulvisk@code.edu.az>
2023-07-09 15:24:59 -0700

    Update's

--- EduHome.UI/ViewComponents/EventFormViewComponent.cs
+++ EduHome.UI/ViewComponents/EventFormViewComponent.cs
@@ -1,5 +1,8 @@
-﻿using EduHomeDataAccess.Database;
+﻿using EduHome.Core.Entities;
+using EduHome.UI.ViewModel;
+using EduHomeDataAccess.Database;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace EduHome.UI.ViewComponents;
 
@@ -13,6 +16,31 @@ public class EventFormViewComponent: ViewComponent
 
 	public async Task<IViewComponentResult> InvokeAsync()
 	{
-		return View();
-	}
+        ViewerViewModel viewerViewModel = new ViewerViewModel();
+        return View(viewerViewModel);
+    }
+
+    [HttpPost]
+    public async Task<IViewComponentResult> SubmitForm(ViewerViewModel viewerViewModel)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View("InvokeAsync", viewerViewModel);
+        }
+
+        Viewer viewer = new Viewer
+        {
+            Name = viewerViewModel.Name,
+            Email = viewerViewModel.Email,
+            Subject = viewerViewModel.Subject,
+            Message = viewerViewModel.Message
+        };
+       
+
+        await _context.Viewers.AddAsync(viewer);
+        await _context.SaveChangesAsync();
+
+        return View("Success"); // Başarılı sonucu temsil eden bir view döndürün
+    }
+
 }
---
+++ EduHome.UI/ViewModel/ViewerViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EduHome.UI.ViewModel;
+
+public class ViewerViewModel
+{
+    public string Name { get; set; } = null!;
+    [Required, MaxLength(70)]
+    public string Email { get; set; } = null!;
+    [Required, MaxLength(100)]
+    public string Subject { get; set; } = null!;
+    [Required, MaxLength(3000)]
+    public string Message { get; set; } = null!;
+}
